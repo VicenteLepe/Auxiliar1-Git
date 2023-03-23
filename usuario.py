5826f86bb05170c740ca4215c4403bd516c704a7
VicenteLepe <vicente.lepe.c@ug.uchile.cl>
2023-03-22 22:19:39 -0300

    conflicto resuelto

--- usuario.py
+++ usuario.py
@@ -7,12 +7,9 @@ class Usuario:
 
     def agregarTarea(self, tarea):
         self.tareas.append(tarea)
-<<<<<<< HEAD
     
     def listarTareas(self):
         for tarea in self.tareas:
             if tarea.estaLista():
                 print(f"[X] {tarea.obtenerNombre()}" )
                 print(f"[ ] {tarea.obtenerNombre()}" )
-=======
->>>>>>> 07dcd9bac5e97ebc30c747d4b337950bab12db34
